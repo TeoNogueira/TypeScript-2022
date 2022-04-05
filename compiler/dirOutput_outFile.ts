cbc3419b962b3dcedd254a357aa7e1f563899dbf
Teo Nogueira <teonogueira17@gmail.com>
2022-04-05 16:44:01 -0300

    finish

---
+++ compiler/dirOutput_outFile.ts
@@ -0,0 +1,10 @@
+// Uma forma de colocar seus arquivos compilados dentro de uma determinada pasta é discomentar a proprieade "outDir" que se encontra dentro do arquivo tsconfig.json 
+
+//ex:
+// "outDir": "./assets"
+
+
+///////
+
+
+// "outDir": "./pastaQualquer/arquivoQualquer.js" <<< before change "module": "commonjs" to "system"
---
+++ compiler/noImplictAny.ts
@@ -0,0 +1,9 @@
+function anyExemple(e, b) {
+    
+    return e + b
+
+};
+
+
+// Sabemos que quando queremos declara qualquer tipo usamos any ex: (e:any, b:any); para evitar está usando a propriedade any declarada, posso ir em noImplictAny e por false,  habilite essa propriedade dentro do arquivo tsconfig.json 
+//
