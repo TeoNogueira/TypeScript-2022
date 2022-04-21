c425e3bbb4e2386a4b3c0e7116af5c5b7f05fc0c
Teo Nogueira <teonogueira17@gmail.com>
2022-04-21 17:46:54 -0300

    Generics

---
+++ generics/functions.generics.ts
@@ -0,0 +1,19 @@
+function kael(obj: any) {
+
+
+    return obj
+} 
+
+const magic:string = '50'
+
+console.log(kael('Hello'))
+console.log(kael({name: 'kael', skillsType: Number(magic), age: 22}))
+
+
+function kaelMore<EG>(O: EG): EG {
+
+    return O
+}
+
+console.log(kaelMore<string>('Hello')) // TYPE REAL
+console.log(kaelMore({name: 'Elune' + ' be Praise! | (ref: Warcraft III).', skillsType: Number(magic), age: 322}))
---
+++ generics/intro_generics.ts
@@ -0,0 +1,11 @@
+function ekko(obj: any) {
+
+
+    return obj
+} 
+
+const magic:string = '50'
+
+
+console.log(ekko('Hello'))
+console.log(ekko({name: 'Ekko', skillsType: Number(magic), age: 22}))
