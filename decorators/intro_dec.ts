ffd0c3b8d7291ece9be043eb7fe35032e056ce87
Teo Nogueira <teonogueira17@gmail.com>
2022-04-21 21:53:39 -0300

    modify_tsconfig_experimentalDecorators

---
+++ decorators/intro_dec.ts
@@ -0,0 +1,19 @@
+@fromDeepClass
+
+/// Set Experimentaldecorators in tsconfig.json 
+
+class Conglomerate {
+
+    constructor() {
+        console.log('Hi!')
+    }
+}
+
+
+function fromDeepClass(build: Function) {
+
+console.log(build)
+
+}
+
+new Conglomerate()
