d8e180d55810aab87933931c53cb308ba32b2dc6
Teo Nogueira <teonogueira17@gmail.com>
2022-04-22 19:14:57 -0300

    Finished_Libraries

---
+++ libraries/library.ts
@@ -0,0 +1,13 @@
+/*
+
+when you see the jquery code "$" the typescript doesn't understand, but it runs in the console even though it doesn't know. In the next classes you will see how to solve this interpretation of a library for typescript.
+$('body').append('Hello World!')
+*/
+
+//Lesson 2 reference Dolar "$" creating jquery.d.ts << resolv problem
+
+
+$('body').append('Hello World!')
+
+
+
