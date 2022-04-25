2185e94e4947c9bc878befbdc40752ac743ceb25
Teo Nogueira <teonogueira17@gmail.com>
2022-04-25 03:07:04 -0300

    deleted init.ts_ training bot

---
+++ init.js
@@ -0,0 +1,25 @@
+const bot = []
+
+
+function msgBot (ask, reply)  {
+
+
+if(ask) {
+
+return bot.push(reply)
+
+}
+
+
+}
+
+
+function recipeValid() {
+
+    msgBot('Seu nome?', 'Me chamo Teo!')
+
+}
+
+recipeValid()
+
+console.log(bot)
